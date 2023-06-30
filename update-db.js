e8c8435726b8d8506ca65ddc5cdd0f1982b34b49
Kathrin <kathrin.ayer@gmail.com>
2023-06-30 10:45:40 -0400

    format record object
    Grab title, add merge date.

--- update-db.js
+++ update-db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { Octokit } = require("@octokit/rest");
+const headerRegex = /(?<prefix># )(?<title>[a-zA-z ]+)/;
 
 const fetchAllChanges = async () => { 
   const octokit = new Octokit({
@@ -23,20 +24,44 @@ const fetchAllChanges = async () => {
               }
               totalCount
             }
+            mergedAt
           }
         }
       }
     }
   }
-  `)
-  
-  console.log(JSON.stringify(result, null, 2))
+  `);
 
-  console.log(result.repository)
+  // TODO: add pagination routine if totalCount > 5
+
+  if (result.repository?.pullRequests?.edges) {
+    const { edges } = result.repository?.pullRequests;
+    const mds = [];
+
+    edges.forEach(({node: {files: {nodes}, mergedAt}}) => {
+      nodes.forEach(({path}) => {
+        const end = path.slice(-3);
+        if (end === '.md') {
+          mds.push({path, mergedAt});
+        }
+      })
+    });
+
+    mds.forEach(async ({path: markdownpath, mergedAt}) => {
+      path = `https://github.com/kayernyc/til/raw/main/${markdownpath}`
+      const stream = await fetch(path).then(async (response) => {
+        
+        const txt = await response.text();
+        const header = headerRegex.exec(txt).groups?.title;
+        
+        // Confirm that record doesn't already exist in DB
+        // Add to DB
+
+        console.log(txt, {header}, mergedAt);
+      });
+    })
+  }
   
-  // for (let { node: { id, files } } of result.data.repository.pullRequests.edges) {
-  //   console.log(`PR #${id} has ${files.totalCount} files changed:`)
-  // }
 }
 
 fetchAllChanges();
